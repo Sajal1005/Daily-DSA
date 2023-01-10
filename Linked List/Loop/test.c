3c3c5fe84de00e736602e5a4b100528b6267e1d7
Sajal1005 <nandasajal.208@gmail.com>
2023-01-10 19:45:13 +0530

    new q's

--- Linked List/Loop/test.c
+++ Linked List/Loop/test.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    char ch;
-    scanf("%c",&ch);
-    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-        printf("%s","it is vowel");
+    // char c;
+    // scanf("%c",&c);
+    // if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+    //     printf("it is character");
+    // }else if(c>='0' && c<='9'){
+    //     printf("%c it is number",c);
+    // }else{
+    //     printf("%c is a special character",c);
+    // }
+    for(int i=1;i<=10;i++){
+        for(int j=3;j<=5;j++){
+            // int ans=i*j;
+            printf("%d*%d=%d ",j,i,i*j);
+        }
+        printf("\n");
     }
+
 }
