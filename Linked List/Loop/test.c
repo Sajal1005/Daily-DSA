55cc205ba2f29e0c29790a792e17bd563ec35c29
Sajal1005 <nandasajal.208@gmail.com>
2023-01-11 21:37:07 +0530

    new q's

--- Linked List/Loop/test.c
+++ Linked List/Loop/test.c
@@ -10,12 +10,45 @@ int main()
     // }else{
     //     printf("%c is a special character",c);
     // }
-    for(int i=1;i<=10;i++){
-        for(int j=3;j<=5;j++){
-            // int ans=i*j;
-            printf("%d*%d=%d ",j,i,i*j);
-        }
-        printf("\n");
-    }
 
+
+    // for(int i=1;i<=10;i++){
+    //     for(int j=3;j<=5;j++){
+    //         // int ans=i*j;
+    //         printf("%d*%d=%d ",j,i,i*j);
+    //     }
+    //     printf("\n");
+    // }
+
+    // int c;
+    // scanf("%d",&c);
+    // int sum=0;
+    // int i=1;
+    // while(i<=c){
+    //     sum+=i;
+    //     i++;
+    // }
+    // printf("%d",sum);
+
+    // int c,ans=1;
+    // scanf("%d",&c);
+    // for(int i=1;i<=c;i++){
+    //     ans*=i;
+    // }
+    // printf("%d",ans);
+
+    int a=0,b=1;
+    int n;
+    scanf("%d",&n);
+    if(n==1){
+        printf(0);
+    }else if(n==2){
+        printf(1);
+    }
+    for(int i=3;i<=n;i++){
+        int temp=b;
+        b+=a;
+        a=temp;
+    }
+    printf("%d",b);
 }
