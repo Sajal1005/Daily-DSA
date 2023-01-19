725e2cd7a511a38d5ce9b871640f70654ab7473c
Sajal1005 <nandasajal.208@gmail.com>
2023-01-19 22:00:19 +0530

    new q's

--- Linked List/test.c
+++ Linked List/test.c
@@ -192,42 +192,42 @@ int main()
     //     break;
     // }
 
-    int n;
-    scanf("%d",&n);
-    int n500=0, n100=0, n50=0, n20=0,n10=0,n5=0,n2=0,n1=0,x=n;
-    if(n>=500){
-        n500 = n/500;
-        n=n%500;
-    }
-    if(n>=100){
-        n100 = n/100;
-        n=n%100;
-    }
-    if(n>=50){
-        n50 = n/50;
-        n=n%50;
-    }
-    if(n>=20){
-        n20 = n/20;
-        n=n%20;
-    }
-    if(n>=10){
-        n10 = n/10;
-        n=n%10;
-    }
-    if(n>=5){
-        n5 = n/5;
-        n=n%5;
-    }
-    if(n>=2){
-        n2 = n/2;
-        n=n%2;
-    }
-    if(n>=1){
-        n1 = n/1;
-        n=n%1;
-    }
-    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n500,n100,n50,n20,n10,n5,n2,n1);
+    // int n;
+    // scanf("%d",&n);
+    // int n500=0, n100=0, n50=0, n20=0,n10=0,n5=0,n2=0,n1=0,x=n;
+    // if(n>=500){
+    //     n500 = n/500;
+    //     n=n%500;
+    // }
+    // if(n>=100){
+    //     n100 = n/100;
+    //     n=n%100;
+    // }
+    // if(n>=50){
+    //     n50 = n/50;
+    //     n=n%50;
+    // }
+    // if(n>=20){
+    //     n20 = n/20;
+    //     n=n%20;
+    // }
+    // if(n>=10){
+    //     n10 = n/10;
+    //     n=n%10;
+    // }
+    // if(n>=5){
+    //     n5 = n/5;
+    //     n=n%5;
+    // }
+    // if(n>=2){
+    //     n2 = n/2;
+    //     n=n%2;
+    // }
+    // if(n>=1){
+    //     n1 = n/1;
+    //     n=n%1;
+    // }
+    // printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n500,n100,n50,n20,n10,n5,n2,n1);
     // float ans=0;
     // if(n<=199){
     //     ans+=(n*1.20);
@@ -255,4 +255,146 @@ int main()
     //     printf("3rd quad");
     // }
 
+    // char c;
+    // scanf("%c",&c);
+    // if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+    //     printf("Alphabet");
+    // }else if(c>='0' && c<='9'){
+    //     printf("Digit");
+    // }else{
+    //     printf("Special");
+    // }
+
+    // int n;
+    // scanf("%d",&n);
+    // int n500=0,n100=0,n50=0,n20=0,n10=0,n5=0,n2=0,n1=0;
+    // if(n>=500){
+    //     n500=n/500;
+    //     n-=(n500*500);
+    // }
+    // if(n>=100){
+    //     n100=n/100;
+    //     n-=(n100*100);
+    // }
+    // if(n>=50){
+    //     n50=n/50;
+    //     n-=(n50*50);
+    // }if(n>=20){
+    //     n20=n/20;
+    //     n-=(n20*20);
+    // }if(n>=10){
+    //     n10=n/10;
+    //     n-=(n10*10);
+    // }if(n>=5){
+    //     n5=n/5;
+    //     n-=(n5*5);
+    // }if(n>=2){
+    //     n2=n/2;
+    //     n-=(n2*2);
+    // }if(n>=1){
+    //     n1=n/1;
+    //     n-=(n1*1);
+    // }
+    // printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n500,n100,n50,n20,n10,n5,n2,n1);
+
+    // float a,b,c;
+    // scanf("%f%f%f",&a,&b,&c);
+    // float d ;
+    // d= sqrt((b*b) - (4*a*c));
+    // float r1 ;
+    // r1= (-b + d) / 2*a;
+    // float r2;
+    // r2 = (-b - d) / 2*a;
+    // printf("%f\n%f",r1,r2);
+
+    // int m,y;
+    // scanf("%d%d",&m,&y);
+    // if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12){
+    //     printf("days are 31");
+    // }else if(m==2){
+    //     if(y%400==0 || (y%4==0 && y%100!=0)){
+    //         printf("days are 29");
+    //     }else{
+    //         printf("days are 28");
+    //     }
+    // }else{
+    //     printf("days are 30");
+
+    // }
+
+    // int n,y=0,w=0,d=0;
+    // scanf("%d",&n);
+    // if(n>=365){
+    //     y=n/365;
+    //     n-=(365*y);
+    // }
+    // if(n>=7){
+    //     w=n/7;
+    //     n-=(7*w);
+    // }
+    // if(n>=1){
+    //     d=n/1;
+    //     n-=(1*d);
+    // }
+    // printf("%d\n%d\n%d",y,w,d);
+
+    // printf("%d", 15>10);
+    // int a,b,c;
+    // scanf("%d%d%d",&a,&b,&c);
+    // switch(a>b){
+    //     case 1:
+    //     switch(a>c){
+    //         case 1:
+    //         printf("a is greatest");
+    //         break;
+    //         default:
+    //         printf("c is greatest");
+    //         break;
+    //     }
+    //     break;
+    // default:
+    // switch (b>c)
+    // {
+    // case 1:
+    //     printf("b is greatest");
+    //     break;
+    
+    // default:
+    // printf("c is greatest");
+    //     break;
+    // }
+    // }
+
+    int x,y;
+    scanf("%d%d",&x,&y);
+    switch (x>0)
+    {
+    case 1:
+        switch (y>0)
+        {
+        case 1:
+            printf("1st quad");
+            break;
+        
+        default:
+        printf("4th quad");
+            break;
+        }
+        break;
+    
+    default:
+    switch (y>0)
+    {
+    case 1:
+        printf("2nd quad");
+        break;
+    
+    default:
+    printf("3rd quad");
+        break;
+    }
+        break;
+    }
+
 }
+
